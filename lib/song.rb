6a63ea26fa43267700288e66faec7ce45c1e87e1
Lydia B <lydiabaik@gmail.com>
2020-01-15 06:29:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -46,10 +46,9 @@ class Song
   def self.new_from_filename(filename)
     split_filename = filename.split(" - ")
     song = self.new
-    song.name = split_filename[1]
+    song.name = split_filename[1].split(".mp3")[0]
     song.artist_name = split_filename[0]
     song
-
   end
 
   # def read_me
