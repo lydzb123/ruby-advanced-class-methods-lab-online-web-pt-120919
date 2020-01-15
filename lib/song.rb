0f71c1040918a71510f990cf277e5594b1c3df89
Lydia B <lydiabaik@gmail.com>
2020-01-15 06:33:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -40,7 +40,7 @@ class Song
   end
 
   def self.alphabetical
-    @@all.sort { |a, b| a.name <=> b.name }
+    self.all.sort { |a, b| a.name <=> b.name }
   end
 
   def self.new_from_filename(filename)
